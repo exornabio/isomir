6f0e1d918a23848e0ccb15ad9ac6adbc2f906420
caibinperl <caibinperl@163.com>
2023-01-13 10:17:12 +0800

    change seq() to seq_len()

--- script/split_reads.R
+++ script/split_reads.R
@@ -6,7 +6,7 @@ split_line_num <- as.integer(args[2])
 read_dir <- args[3]
 
 reads <- read_tsv(read_file, col_names = FALSE, show_col_types = FALSE)
-reads <- split(reads, ceiling(seq(nrow(reads)) / split_line_num))
+reads <- split(reads, ceiling(seq_len(nrow(reads)) / split_line_num))
 
 for (chunk in names(reads)) {
   write_tsv(reads[[chunk]],
