31130d62e96cfffb3af2653d6ba896d7cc5d2936
caibinperl <caibinperl@163.com>
2023-01-20 16:36:32 +0800

    split reads by file number

--- script/split_reads.R
+++ script/split_reads.R
@@ -2,10 +2,11 @@ suppressMessages(library(tidyverse))
 
 args <- commandArgs(TRUE)
 read_file <- args[1]
-split_line_num <- as.integer(args[2])
+split_num <- as.integer(args[2])
 read_dir <- args[3]
 
 reads <- read_tsv(read_file, col_names = FALSE, show_col_types = FALSE)
+split_line_num <- floor(nrow(reads) / split_num)
 reads <- split(reads, ceiling(seq_len(nrow(reads)) / split_line_num))
 
 for (chunk in names(reads)) {
